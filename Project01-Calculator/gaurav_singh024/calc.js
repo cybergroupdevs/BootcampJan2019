aaabc6f21ec17ffd847d769dc48f2b9e2cb7a0e6
gaurav singh <gauravsg211@gamil.com>
2019-01-24 23:38:25 +0530

    added proper css in the existing file

--- Project01-Calculator/gaurav_singh024/calc.js
+++ Project01-Calculator/gaurav_singh024/calc.js
@@ -1,16 +1,42 @@
-function dis(val) 
+
+
+  
+
+      
+ 
+  
+    
+     function dis(val) 
+
+     
+
     { 
 document.getElementById("result").value+=val 
-}       
+} 
+
+           
+
+      
+  
+         
 
 function solve() 
-  { 
+
+        
+ { 
 
  x = document.getElementById("result").value 
-  y = eval(x) ;
-document.getElementById("result").value = y } 
 
- function clr()
+           
+
+  y = eval(x) 
+
+            
+ document.getElementById("result").value = y } 
+
+
+
+   function clr()
  
      { document.getElementById("result").value = "" 
 
