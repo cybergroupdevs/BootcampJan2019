2b90120f023f087852c0f99b816cfadca16e7054
Ravi Kant Sharma <ravikantsharma2169@gmail.com>
2019-01-26 15:28:08 +0530

    Website Layout changed, background updated, dummy js code added

---
+++ Week1-Project/RAVISHARMA022/js/user details.js
@@ -0,0 +1,32 @@
+   <script
+  src="http://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+            crossorigin="anonymous"></script>
+        
+        <script>
+                function getEmployees(){
+                    
+                    var datatosend ={
+                                    
+                                        "emp_name": "Taran",
+                                        "age": 25,
+                                        "gender": "male",
+                                        "DesignationId": 3
+                                        
+                                    }
+                    
+                    $.ajax({
+                        url: "http://localhost:52041/api/Employees",
+                        data: datatosend,
+                        type: 'post',
+                        dataType: 'json', 
+                        success: function(res){
+                        console.log(res);
+                        alert();
+                        }   
+                        
+                    })
+                    
+                };
+    </script>
+    
