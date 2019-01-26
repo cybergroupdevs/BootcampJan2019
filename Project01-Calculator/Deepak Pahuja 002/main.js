90199736a3adefbf87e5be1587e8c5556a978750
DeepakPahuja96 <46512707+DeepakPahuja96@users.noreply.github.com>
2019-01-26 16:50:25 +0530

    comment unwanted statements

--- Project01-Calculator/Deepak Pahuja 002/main.js
+++ Project01-Calculator/Deepak Pahuja 002/main.js
@@ -1,11 +1,11 @@
 var result = document.getElementById("result");
 var button = document.getElementsByClassName("button");
 
-var funcToButton = ['1','2']
+// var funcToButton = ['1','2']
 
-for (i = 0; i < 10; 1++){
-    button[i].addEventListener()
-}
+// for (i = 0; i < 10; 1++){
+//     button[i].addEventListener()
+// }
 
 
 function myfunc(val){
