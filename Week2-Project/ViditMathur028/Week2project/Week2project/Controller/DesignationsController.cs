e35892fde3f22b7d16629a5f098b189befa27176
Vidit Mathur <mathur.vidit123@gmail.com>
2019-02-07 13:06:29 +0530

    week 2 backend updated

---
+++ Week2-Project/ViditMathur028/Week2project/Week2project/Controller/DesignationsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Week2project.Model;
+
+namespace Week2project.Controller
+{
+    public class DesignationsController : ApiController
+    {
+        private CorporateEntities5 db = new CorporateEntities5();
+
+        // GET: api/Designations
+        public IQueryable<Designation> GetDesignations()
+        {
+            return db.Designations;
+        }
+
+        // GET: api/Designations/5
+        [ResponseType(typeof(Designation))]
+        public IHttpActionResult GetDesignation(int id)
+        {
+            Designation designation = db.Designations.Find(id);
+            if (designation == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(designation);
+        }
+
+        // PUT: api/Designations/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDesignation(int id, Designation designation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != designation.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(designation).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DesignationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Designations
+        [ResponseType(typeof(Designation))]
+        public IHttpActionResult PostDesignation(Designation designation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Designations.Add(designation);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (DesignationExists(designation.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = designation.Id }, designation);
+        }
+
+        // DELETE: api/Designations/5
+        [ResponseType(typeof(Designation))]
+        public IHttpActionResult DeleteDesignation(int id)
+        {
+            Designation designation = db.Designations.Find(id);
+            if (designation == null)
+            {
+                return NotFound();
+            }
+
+            db.Designations.Remove(designation);
+            db.SaveChanges();
+
+            return Ok(designation);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DesignationExists(int id)
+        {
+            return db.Designations.Count(e => e.Id == id) > 0;
+        }
+    }
+}
