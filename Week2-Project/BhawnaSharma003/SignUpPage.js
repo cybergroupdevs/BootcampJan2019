816dc51a8bdf1a70a761c2f5cde725bde90ea1d4
Bhawna Sharma <46512586+bhawna22@users.noreply.github.com>
2019-02-15 00:23:40 +0530

    Modified some validations

--- Week2-Project/BhawnaSharma003/SignUpPage.js
+++ Week2-Project/BhawnaSharma003/SignUpPage.js
@@ -1,53 +1,57 @@
 function validation(){
-    var user = document.getElementById('name').value;
-    var user = document.getElementById('employmentid').value;
-    var user = document.getElementById('email').value;
+    var name = document.getElementById('name').value;
+    var employmentid = document.getElementById('employmentid').value;
+    var email = document.getElementById('email').value;
     var password = documnet.getElementById("password").value;
-    var user = document.getElementById('confirmpassword').value;
+    var confirmpassword = document.getElementById('confirmpass').value;
 
     if (name ==""){
-        document.getElementById("name").innerHTML = "** Please fill the name details";
-        return false;   
+         alert("Please fill the name");
+         return false; 
     }
-    if((name.length <=2) || (name.length>20)) {
-        document.getElementById("name").innerHTML = "** Usename must be between 2 and 20";
-        return false;   
+    else if((name.length <=2) || (name.length>20)) {
+         alert( "Username must be between 2 and 20");
+         return false;
     }
-    if(!isNAN(name)){
-        document.getElementById("name").innerHTML = "** Only characters are allowed";
-        return false;   
+    else if(!isNAN(name)){
+         alert("Only characters are allowed");
+         return false;   
     }
-    if (employmentid ==""){
-        document.getElementById("email").innerHTML = "** Please fill the Employment Id details";
-        return false;   
+    else if (employmentid ==""){
+         alert("Please fill the Employment Id details");
+         return false;  
     }
-    if (email ==""){
-    	document.getElementById("email").innerHTML = "** Please fill the email details";
-    	return false;	
+    else if (email ==""){
+    	 alert("Please fill the email details");	
+         return false;	
     }
-    if (email.indexOf('@')<=0){
-    	document.getElementById("email").innerHTML = "** @ Invalid Position";
-    	return false;	
+    else if (email.indexOf('@')<=0){
+    	 alert("Invalid @ Position");
+         return false;	
     }
-    if((email.charAt(email.length-4)!='.') && (email.charAt(email.length-3)!='.')){
-    	document.getElementById("email").innerHTML = "** . Invalid Position";
-    	return false;
+    else if((email.charAt(email.length-4)!='.') && (email.charAt(email.length-3)!='.')){
+    	 alert("Invalid dot Position");
+         return false;
     }
-    if (password ==""){
-    	document.getElementById("password").innerHTML = "** Please fill the password details";
-    	return false;	
+    else if (password ==""){
+    	 alert("Please fill the password details");
+         return false;	
     }
-    if((password.length <=5)||(password.length>20)){
-    	document.getElementById("password").innerHTML = "** Password length must be between 5 and 20";
-    	return false;	
+    else if((password.length <=5)||(password.length>20)){
+    	 alert("Password length must be between 5 and 20");
+         return false;
     }
-    if (confirmpassword ==""){
-        document.getElementById("email").innerHTML = "** Please confirm the password details";
-        return false;   
+    else if (confirmpassword ==""){
+         alert("Please confirm the password details");
+         return false;  
     }
-    if (password!=confirmpassword){
-        document.getElementById("email").innerHTML = "** Passwords do not match";
-        return false;   
+    else if (password!=confirmpassword){
+         alert("Passwords do not match");
+         return false;
+    }
+    else{
+        window.location="EnterDetails.html";
+        return true;
     }
 
 }
