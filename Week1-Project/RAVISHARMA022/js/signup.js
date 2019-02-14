e361aaeffc72003f06c44ce3fb581fe74d3edcb0
Ravi Kant Sharma <ravikantsharma2169@gmail.com>
2019-02-15 01:05:15 +0530

    Signup fucntionality changed

--- Week1-Project/RAVISHARMA022/js/signup.js
+++ Week1-Project/RAVISHARMA022/js/signup.js
@@ -1,61 +1,72 @@
-function signupform(){
-	var firstname = document.getElementById("First Name").value;
+function signupform(){	
+	var employeeid = document.getElementById("EmployeeId").value;
+	var empid=employeeid.valueOf();
+	var firstname = document.getElementById("FirstName").value;
 	var fname=firstname.valueOf();
-	var lastname = document.getElementById("Last Name").value;
+	var lastname = document.getElementById("LastName").value;
 	var lname=lastname.valueOf();
-	var Email = document.getElementById("Email").value;
-	var mail=Email.valueOf();
-	var pass = document.getElementById("Password").value;
-	var pass1=pass.valueOf();
-	var repass = document.getElementById("Repassword").value;
-	var pass2=repass.valueOf();
+	var email = document.getElementById("Email").value;
+	var mail=email.valueOf();
+	var password = document.getElementById("Password").value;
+	var pass=password.valueOf();
+	var repassword = document.getElementById("Repassword").value;
+	var repass=repassword.valueOf();
+	var phone = "NA";
+	var graddegree = "NA";
+	var gradmarks = 0;
+	var twelfthboard = "NA";
+	var twelfthmarks = 0;
+	var tenthboard = "NA";
+	var tenthmarks = 0;
 	var country = document.getElementById("Country").value;
 	var region=country.valueOf();
-	var employeeid = document.getElementById("Employee Id").value;
-	var empid=employeeid.valueOf();
 	var dob = document.getElementById("dob").value;
+	var fathername = "NA";
+	var gender = "NA";
+	var designation = "NA";
+	var address = "NA";
 	var terms = document.getElementById("licence").value;
-	var licence=terms.valueOf();
-if(pass1 == pass2 && licence == 1 ){
-	var datafor_EmployeeDetail ={
+	var licence=parseInt(terms);
+	if(pass == repass && licence == 1 ){
+			var datafor_EmployeeDetail={
 								"Id": empid,
 								"firstname":fname,
 								"lastname":lname,
-								"fathername": "Not Specified",
+								"fathername": fathername,
 								"Email":mail,
-								"Phone": "Not Specified",
-								"country": region,
+								"Phone": phone,
+								"country":region,
 								"dob":dob, 
-								"gender": "NA",
-								"designation": "Intern",
-								"address": "Needs to be updated",
-								"AuthenticateUser": {
+								"gender": gender,
+								"designation": designation,
+								"address": address,
+								"EducationDetail": {
+								"Id": empid,
+								"TenthBoard": tenthboard,
+								"Tenthmarks": tenthmarks,
+								"Twelfthboard": twelfthboard,
+								"Twelfthmarks": twelfthmarks,
+								"GraduationDegree": graddegree,
+								"GraduationMarks": gradmarks,
+								},
+								"AuthenticateUser":{
 								"Id": empid,
 								"Email": mail,
-								"password": pass1,
+								"password": pass,
 								"Terms": licence,
 								},
-								"EducationDetail":{
-								"GraduationDegree": "Not Specified",
-								"GraduationMarks": 0,
-								"Id": empid,
-								"TenthBoard": "Not Specified",
-								"Tenthmarks": 0,
-								"Twelfthboard": "Not Specified",
-								"Twelfthmarks": 0,	
-										},
 								};
-						$.ajax	({
-							url: "http://localhost:50052/api/EmployeeDetail",
+						$.ajax({
+							url: 'http://localhost:50052/api/EmployeeDetail/',
 							data: datafor_EmployeeDetail,
-							type: 'post',
+							type: 'POST',
 							dataType: 'json', 
-							success: function(res){ 
-													alert("You've been signed up successfully, Please Login to continue");
-													window.location="index.html";
-												}
+							success: function(response){ 
+													alert("SignUp Successful, Please Login To Continue");
+													window.location="user list.html";
+													}	
 								});
-		}else{alert("Password doesn't match");
+			}else{alert("Password doesn't match");
 			window.location.href = "Signup.html";}
 };
 
