0af0665736bcd99c5ece746a5d0f0c9cf3f2d42f
Bhawna Sharma <46512586+bhawna22@users.noreply.github.com>
2019-02-15 12:32:34 +0530

    Minor Changes in the validation

--- Week2-Project/BhawnaSharma003/LoginPage.js
+++ Week2-Project/BhawnaSharma003/LoginPage.js
@@ -1,26 +1,30 @@
 function validation(){
-    var user = document.getElementById('email').value;
-    var password = documnet.getElementById("password").value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById("password").value;
 
     if (email ==""){
-    	document.getElementById("email").innerHTML = "** Please fill the email details";
+    	alert(" Please fill the email details");
     	return false;	
     }
-    if (email.indexOf(@)<=0){
-    	document.getElementById("email").innerHTML = "** @ Invalid Position";
+    else if (email.indexOf(@)<=0){
+    	alert(" Invalid @ Position");
     	return false;	
     }
-    if((email.charAt(email.length-4)!='.') && (email.charAt(email.length-3)!='.')){
-    	document.getElementById("email").innerHTML = "** . Invalid Position";
+    else if((email.charAt(email.length-4)!='.') && (email.charAt(email.length-3)!='.')){
+    	alert(" Invalid Dot Position");
     	return false;
     }
-    if (password ==""){
-    	document.getElementById("password").innerHTML = "** Please fill the password details";
+    else if (password ==""){
+    	alert (" Please fill the password details");
     	return false;	
     }
-    if(password.length <=5)||(password.length>20){
-    	document.getElementById("password").innerHTML = "** Password length must be between 5 and 20";
+    else if((password.length <=5)||(password.length>20)){
+    	alert("Password length must be between 5 and 20");
     	return false;	
     }
+   else {
+        window.location="EnterDetails.html";
+        return true;
+   }
 
 }
